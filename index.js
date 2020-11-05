7a06d9cf4e1d9c4e9637ed30fa4cfc549f39a71f
falstack <icesilt@outlook.com>
2020-11-05 14:38:05 +0800

    upgrade

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { createStore, createComponent } from './src/store'
-import Component from './src/component'
+import * as utils from './src/component'
 
 export default {
   createStore,
   createComponent,
-  Component
+  utils
 }
