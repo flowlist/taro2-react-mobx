cf65256e0b39ce8f1dfe0a3d5144288c9fb997d2
falstack <icesilt@outlook.com>
2020-11-08 12:13:49 +0800

    add: scrollview support

--- src/store.js
+++ src/store.js
@@ -36,21 +36,41 @@ export const createStore = () => new Store()
 
 export const reactive = observer
 
-export const didMount = (self, {
-  className
-}) => {
+export const didMount = (self, opts = {}) => {
+  if (!opts.className) {
+    return
+  }
+
   self.ob = Taro.createIntersectionObserver(self.$scope, {
     thresholds: [0]
   })
 
-  self.ob.relativeToViewport({ bottom: 0 }).observe(`.${className}`, (e) => {
+  self.ob.relativeToViewport({ bottom: 0 }).observe(`.${opts.className}`, (e) => {
     if (e.intersectionRatio <= 0) {
       return
     }
-    if (self.props.store.state && self.props.store.state.fetched) {
-      self.props.store.loadMore(self.props.params)
-    } else {
+
+    if (!self.props.store.state.fetched) {
       self.props.store.initData(self.props.params)
+      return
+    }
+
+    if (!opts.scrollView) {
+      self.props.store.loadMore(self.props.params)
+    }
+  })
+}
+
+export const scrollBottomFn = (self) => {
+  self.props.store.loadMore(self.props.params)
+}
+
+export const scrollTopFn = (self) => {
+  self.props.store.loadMore({
+    ...self.props.params,
+    query: {
+      ...(self.props.params.query || {}),
+      is_up: 1
     }
   })
 }
@@ -61,6 +81,8 @@ export const didUnmount = (self) => {
 
 export const defaultProps = {
   launch: true,
+  bottom: true,
+  append: false,
   scrollX: false,
   displayNoMore: false,
   store: {
